eb1a6bf125074f944d8262801a63d6e0d04cc5ca
Matt Brown <mbrown_89@icloud.com>
2026-06-17 15:30:07 +0100

    Add setup command

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ func main() {
 
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(reportCmd)
+	rootCmd.AddCommand(setupCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
---
+++ setup.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var workspaceURL string
+
+var setupCmd = &cobra.Command{
+	Use:   "setup",
+	Short: "Configure Workbrew CLI",
+	Run: func(cmd *cobra.Command, args []string) {
+		runSetup()
+	},
+}
+
+func runSetup() {
+	if workspaceURL == "" {
+		fmt.Println("Missing required --url")
+		return
+	}
+
+	fmt.Println("Setup not implemented yet")
+	fmt.Println("Workspace URL:", workspaceURL)
+}
+
+func init() {
+	setupCmd.Flags().StringVar(
+		&workspaceURL,
+		"url",
+		"",
+		"Workbrew workspace URL",
+	)
+}
