e42c397b1bd21ee4bbe31450d87c45676576cdb4
Matt Brown <mbrown_89@icloud.com>
2026-06-17 11:49:44 +0100

    Added in cobra framework

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const version = "0.1.0"
+
+func getVersion() string {
+	return version
+}
 
 func main() {
-	version := "0.1.0"
+	var rootCmd = &cobra.Command{
+		Use:   "workbrew",
+		Short: "A CLI for querying Workbrew",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Workbrew CLI")
+		},
+	}
 
-	fmt.Println("Workbrew CLI")
-	fmt.Println("Version:", version)
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the application version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(getVersion())
+		},
+	}
+
+	rootCmd.AddCommand(versionCmd)
+
+	rootCmd.Execute()
 }
