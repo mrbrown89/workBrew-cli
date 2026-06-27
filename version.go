3fde5e4625c3861baca8acb6f4a44d92167e7647
Matt <mbrown_89@icloud.com>
2026-06-27 21:33:17 +0100

    Added cobra function in

--- version.go
+++ version.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
 const version = "0.3.1"
 
 func getVersion() string {
 	return version
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the application version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(getVersion())
+	},
 }
